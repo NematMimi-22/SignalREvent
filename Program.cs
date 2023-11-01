6ba817018486790c5199329e92b2b2492a273016
Nemat Mimi <nematmimi01@gmail.com>
2023-11-01 14:53:55 +0200

    Edit Main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Weather_Monitoring.ReadConfig;
+namespace SignalREvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var signalRConfig = ConfigReader.ReadSignalRConfig();
+            var signalRClient = new SignalRClient(signalRConfig.SignalRUrl);
+
+            try
+            {
+                signalRClient.StartAsync().GetAwaiter().GetResult();
+                Console.WriteLine("SignalR client connected. Press 'Q' to quit the application.");
+                while (true)
+                {
+                    var eventData = signalRClient.AwaitEvent();
+
+                    if (Console.KeyAvailable)
+                    {
+                        var key = Console.ReadKey(intercept: true).Key;
+                        if (key == ConsoleKey.Q)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                signalRClient.StopAsync().GetAwaiter().GetResult();
+            }
+        }
+    }
+}
---
+++ SignalRClient.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNetCore.SignalR.Client;
+namespace SignalREvent
+{
+    public class SignalRClient
+    {
+        private readonly Microsoft.AspNetCore.SignalR.Client.HubConnection _hubConnection;
+
+        public SignalRClient(string signalRUrl)
+        {
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl(signalRUrl)
+                .Build();
+
+            _hubConnection.On<string>("ReceiveMessage", (message) =>
+            {
+                Console.WriteLine($"Received message: {message}");
+            });
+        }
+
+        public async Task StartAsync()
+        {
+            try
+            {
+                await _hubConnection.StartAsync();
+                Console.WriteLine("Connected to SignalR hub.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error connecting to SignalR hub: {ex.Message}");
+            }
+        }
+
+        public async Task StopAsync()
+        {
+            try
+            {
+                await _hubConnection.StopAsync();
+                Console.WriteLine("Connection to SignalR hub stopped.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error stopping SignalR hub connection: {ex.Message}");
+            }
+        }
+
+        public async Task<string> AwaitEvent()
+        {
+            var completionSource = new TaskCompletionSource<string>();
+            _hubConnection.On<string>("YourEventName", eventData =>
+            {
+                completionSource.SetResult(eventData);
+            });
+            return await completionSource.Task;
+        }
+    }
+}
